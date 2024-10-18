f98979a112aa4845a5b48a193e2079fb8d208713
Pasindu shanuka <143288490+PShanuKa@users.noreply.github.com>
2024-10-18 10:24:40 +0530

    update

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 const baseQuery = fetchBaseQuery({ 
-	baseUrl: 'http://localhost:3000/api',
+	baseUrl: import.meta.env.VITE_API_URL,
 	prepareHeaders: (headers) => {
 	  const token = localStorage.getItem('Bearer') || ''
 	  
